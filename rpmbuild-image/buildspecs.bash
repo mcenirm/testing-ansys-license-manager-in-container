d9048b28167fce836d782c543d1323e15364131c
Michael McEniry <mcenirm@mceniry.net>
2023-08-07 16:21:04 -0500

    prepare rpmbuild image

---
+++ rpmbuild-image/buildspecs.bash
@@ -0,0 +1,14 @@
+set -euo pipefail
+[[ "${TRACE-0}" == "1" ]] && set -x
+
+rpmdev-setuptree
+
+cp -nv -t $HOME/rpmbuild/SPECS /rpmspecs/*.spec
+cp -nv -t $HOME/rpmbuild/SOURCES /rpmsources/*
+
+for s in $HOME/rpmbuild/SPECS/*.spec
+do
+  rpmlint $s
+  rpmbuild -ba $s
+  rsync -rOi $HOME/rpmbuild/RPMS $HOME/rpmbuild/SRPMS /rpmoutputs/
+done
